13f7ec54a5f98363072ad29b282c7f1177d3dcb2
RishithSuresh <rishithsuresh10@gmail.com>
2025-08-25 23:42:33 +0530

    3rd commit

--- Catch_the_ball.py
+++ Catch_the_ball.py
@@ -10,8 +10,13 @@ black = (0, 0, 0)
 white = (255, 255, 255)
 red = (255, 0, 0)
 
+#load images
 basket = pygame.image.load("basket.png")
 ball = pygame.image.load("ball.png")
+
+#resize images
+basket = pygame.transform.scale(basket, (100, 60))
+ball = pygame.transform.scale(ball, (40, 40))
 
 clock = pygame.time.Clock()
 
